1d8c6908bb05c2d78426adbde92a375896921057
AtarMor <159138223+AtarMor@users.noreply.github.com>
2024-02-07 18:52:22 +0200

    version 3 - including recursion

--- js/app.js
+++ js/app.js
@@ -10,9 +10,12 @@ var gLevel = {
     size: 4,
     mines: 2
 }
+var gTimerInterval
 
 function onInit() {
     hideModal()
+    clearInterval(gTimerInterval)
+    /// set time to 00:00
     gGame = {
         isOn: true,
         shownCount: 0,
@@ -23,6 +26,7 @@ function onInit() {
     // setMinesLocations()
     setMinesNegsCount()
     renderBoard()
+    console.table(gBoard)
 }
 
 function buildBoard() {
@@ -88,28 +92,20 @@ function setMinesLocations() {
 
 function onCellClicked(elCell, i, j) {
     if (!gGame.isOn) return
+    if (gBoard[i][j].isMine && gGame.shownCount === 0) {
+        onInit()
+    }
     if (gBoard[i][j].isShown) return
     if (gBoard[i][j].isMine && gBoard[i][j].isMarked) return
 
-    gBoard[i][j].isShown = true
-    gGame.shownCount++
-    elCell.classList.add('shown')
-
-    console.log('gGame.shownCount:', gGame.shownCount)
-
-    var cellValue
     if (gBoard[i][j].isMine) {
-        cellValue = MINE
-    } else if (gBoard[i][j].minesAroundCount > 0) {
-        cellValue = gBoard[i][j].minesAroundCount
+        renderCell({ i, j }, MINE)
+        elCell.classList.add('mine')
+        return gameOver()
     } else {
-        cellValue = ''
         expandShown(i, j)
     }
 
-    renderCell({ i, j }, cellValue)
-
-    if (gBoard[i][j].isMine) return gameOver()
     checkVictory()
 }
 
@@ -129,23 +125,27 @@ function onCellMarked(ev, i, j) {
 }
 
 function expandShown(rowIdx, colIdx) {
+    if (rowIdx < 0 || rowIdx >= gBoard.length) return
+    if (colIdx < 0 || colIdx >= gBoard[0].length) return
+    if (gBoard[rowIdx][colIdx].isShown) return
+
+    gBoard[rowIdx][colIdx].isShown = true
+    gGame.shownCount++
+    startTimer()
+    console.log('gGame.shownCount:', gGame.shownCount)
+    const elCell = document.querySelector(`.cell-${rowIdx}-${colIdx}`)
+    elCell.classList.add('shown')
+
+    if (gBoard[rowIdx][colIdx].minesAroundCount) {
+        renderCell({ i: rowIdx, j: colIdx }, gBoard[rowIdx][colIdx].minesAroundCount)
+        return
+    }
+    renderCell({ i: rowIdx, j: colIdx }, '')
+
     for (let i = rowIdx - 1; i <= rowIdx + 1; i++) {
-        if (i < 0 || i >= gBoard.length) continue
         for (let j = colIdx - 1; j <= colIdx + 1; j++) {
             if (i === rowIdx && j === colIdx) continue
-            if (j < 0 || j >= gBoard[0].length) continue
-            // console.log('gBoard[i][j]:',i, j, gBoard[i][j])
-            gBoard[i][j].isShown = true
-            gGame.shownCount++
-            // console.log('EX-gGame.shownCount', gGame.shownCount)
-            const elCell = document.querySelector(`.cell-${i}-${j}`)
-            elCell.classList.add('shown')
-            if (gBoard[i][j].minesAroundCount) {
-            renderCell({ i, j }, gBoard[i][j].minesAroundCount)
-            } else {
-                renderCell({ i, j }, '')
-                // expandShown(i, j)
-            } 
+            expandShown(i, j)
         }
     }
 }
@@ -161,6 +161,7 @@ function gameOver() {
 }
 
 function endGame(msg) {
+    clearInterval(gTimerInterval)
     const elModalH3 = document.querySelector('.modal h3')
     elModalH3.innerText = msg
     showModal()
@@ -177,3 +178,27 @@ function renderCell(location, value) {
     const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
     elCell.innerHTML = value
 }
+
+function startTimer() {
+    if (gGame.shownCount === 1) {
+        var startTime = Date.now()
+        clearInterval(gTimerInterval)
+        gTimerInterval = setInterval(() => {
+            const timeDiff = Date.now() - startTime
+            const seconds = getSeconds(timeDiff)
+            const minutes = getMinutes(timeDiff)
+            document.querySelector('span.seconds').innerText = seconds
+            document.querySelector('span.minutes').innerText = minutes
+        }, 1000)
+    }
+}
+
+    function getSeconds(timeDiff) {
+        const seconds = new Date(timeDiff).getSeconds()
+        return (seconds + '').padStart(2, '0')
+    }
+
+    function getMinutes(timeDiff) {
+        const minutes = new Date(timeDiff).getMinutes()
+        return (minutes + '').padStart(2, '0')
+    }
